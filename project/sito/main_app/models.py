b990ee1cbf4231e6b180ece373642ede8df3a45f
pelulais <pelutienda@gmail.com>
2025-10-28 15:45:24 -0300

    añadí la función de añadir miembros al admin de django

--- project/sito/main_app/models.py
+++ project/sito/main_app/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
-class main_app(models.Model):
-    field1 = models.CharField(max_length=255)
-    field2 = models.CharField(max_length=255)
+class Miembros(models.Model):
+    Nombre = models.CharField(max_length=255)
+    Apellido = models.CharField(max_length=255)
+    numero = models.IntegerField(null=True)
+    fecha_union = models.DateField(null=True)
+
+    def __str__(self):
+        return f"({self.Nombre}{self.Apellido})"
