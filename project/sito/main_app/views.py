0a89bedcd192b1fd41513f9cf0bed010ae311502
mat885 <mati.hugo.g@gmail.com>
2025-10-31 00:34:15 -0300

    Implement user authentication views
    Added user registration, login, and logout views.

--- project/sito/main_app/views.py
+++ project/sito/main_app/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 
 def main_app(request):
     template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+    context = {'user': request.user}
+    return HttpResponse(template.render(context, request))
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    template = loader.get_template('register.html')
+    return HttpResponse(template.render({'form': form}, request))
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render({'form': form}, request))
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
