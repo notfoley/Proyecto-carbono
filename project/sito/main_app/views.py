3a2f1ca433198ef5878eb8905ff7b2783d64103a
mat885 <mati.hugo.g@gmail.com>
2025-10-31 01:00:30 -0300

    Replace 'render' with 'redirect' in views.py

--- project/sito/main_app/views.py
+++ project/sito/main_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 
 def main_app(request):
