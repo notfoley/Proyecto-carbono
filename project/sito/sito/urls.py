d469971080991a00fd485236485ed783b7468342
pelulais <pelutienda@gmail.com>
2025-11-16 23:10:13 -0300

    Avance CRUD 4

--- project/sito/sito/urls.py
+++ project/sito/sito/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
-    path('tasks', views.tasks, name='tasks')
+    path('tasks', views.tasks, name='tasks'),
+    path('logout', views.signout, name='logout'),
+    path('login', views.log_in, name='login')
 ]
